98d154fe1520ad72754bfed0ce041763175ab945
mtraina <matteo.traina.mail@gmail.com>
2018-10-02 22:30:21 +0200

    ex. 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,12 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return if(this.year < other.year) -1
+            else if(this.month < other.month) -1
+                else if(this.dayOfMonth < other.dayOfMonth) -1 else 1
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
--- test/iii_conventions/N25ComparisonKtTest.kt
+++ test/iii_conventions/N25ComparisonKtTest.kt
@@ -5,6 +5,14 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class N25ComparisonKtTest {
+    /*@Test fun test(){
+        listOf(1,2,3,4,5,6).map { e -> println(e); e * 2 }.first()
+        listOf(1,2,3,4,5,6).asSequence().map { e -> println(e); e * 2 }.first()
+
+        val (param, value) = "param=car".split("=")
+        println("param: $param, value: $value")
+    }*/
+
     /* Month numbering starts with 0 (0-Jan, 1-Feb, ... 11-Dec) */
     @Test fun testDateComparison() {
         assertTrue(task25(MyDate(2014, 1, 1), MyDate(2014, 1, 2)))
