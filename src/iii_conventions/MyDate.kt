0ea2e1021f357e4731e6795a2e389e998d43b7a1
mtraina <matteo.traina.mail@gmail.com>
2018-10-02 22:35:22 +0200

    ex. 25 refactored

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -2,9 +2,12 @@ package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
     override fun compareTo(other: MyDate): Int {
-        return if(this.year < other.year) -1
-            else if(this.month < other.month) -1
-                else if(this.dayOfMonth < other.dayOfMonth) -1 else 1
+        return when {
+            this.year < other.year -> -1
+            this.month < other.month -> -1
+            this.dayOfMonth < other.dayOfMonth -> -1
+            else -> 1
+        }
     }
 }
 
